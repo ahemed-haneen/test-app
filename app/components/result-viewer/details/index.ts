079f20321c4dd99cb9bafd1ebcd3f67ec126fe38
ahemed-haneen <haneen.mavoor@gmail.com>
2023-04-13 01:47:19 +0530

    feat(sampleDetails): implement sample details page

---
+++ app/components/result-viewer/details/index.ts
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import Sample from 'drug-report/data-models/sample';
+
+interface ResultViewerDetailsArgs {
+    details: Sample
+}
+export default class ResultViewerDetailsComponent extends Component<ResultViewerDetailsArgs> {
+}
