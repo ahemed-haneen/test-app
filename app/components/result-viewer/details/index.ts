5757289f30ba8a76d6822b033b520dfe7eba0c9a
Haneen Ahemed A <haneen.mavoor@gmail.com>
2023-04-14 12:48:52 +0000

    feat(reportDetails): add report table implementation

--- app/components/result-viewer/details/index.ts
+++ app/components/result-viewer/details/index.ts
@@ -1,27 +1,74 @@
-import Component from '@glimmer/component';
-import Sample from 'drug-report/data-models/sample';
+import Component from "@glimmer/component";
+import Sample from "drug-report/data-models/sample";
+import { action } from "@ember/object";
+import { debounce } from "@ember/runloop";
 
 interface ResultViewerDetailsArgs {
-    details: Sample
-}
-export default class ResultViewerDetailsComponent extends Component<ResultViewerDetailsArgs> {
-    columns = [{
-        name: "DRUG CLASS",
-        value: "drug_class"
-    }, 
-{
-    name: 'DRUG',
-    value: 'drugs',
-},
-{
-    name: 'CALL',
-    value: 'drugs',
-},{
-name: 'MUTATIONS',
-value: 'mutations'}]
-get rows(){
-    if (this.args.details)
-    return this.args.details.drug_call_result.drug_calls.map((row: {drug_class: string, drugs: Array<any>, mutations: Array<string>}) => { return {drug_class: row.drug_class, mutations: row.mutations, drugs: row.drugs}})
-else return []}
+  details: Sample;
 }
 
+export default class ResultViewerDetailsComponent extends Component<ResultViewerDetailsArgs> {
+  columns = [
+    {
+      name: "DRUG CLASS",
+      valuePath: "drug_class",
+      minWidth: 250,
+      maxWidth: 800,
+    },
+    {
+      name: "DRUG",
+      valuePath: "drugs",
+      maxWidth: 90,
+    },
+    {
+      name: "CALL",
+      valuePath: "drugs",
+      maxWidth: 90,
+    },
+    {
+      name: "MUTATIONS",
+      valuePath: "mutations",
+      minWidth: 400,
+      maxWidth: 800,
+    },
+  ];
+
+  get rows() {
+    if (this.args.details)
+      return this.args.details.drug_call_result.drug_calls.map(
+        (row: {
+          drug_class: string;
+          drugs: Array<any>;
+          mutations: Array<string>;
+        }) => {
+          return {
+            drug_class: row.drug_class,
+            mutations: row.mutations.join(", "),
+            drugs: row.drugs,
+          };
+        }
+      );
+    else return [];
+  }
+
+  @action
+  didInsert() {
+    window.addEventListener("resize", this.updateListHeight);
+
+    this.updateListHeight();
+  }
+
+  @action
+  updateListHeight() {
+    debounce(this, this.setListHeight, 200);
+  }
+
+  @action
+  setListHeight() {
+    let reportElement = document.querySelector(
+      "#reports-container"
+    ) as HTMLElement;
+    let sampleList = document.querySelector("#sample-list") as HTMLElement;
+    sampleList.style.height = reportElement?.offsetHeight.toString() + "px";
+  }
+}
--- app/components/result-viewer/list/index.ts
+++ app/components/result-viewer/list/index.ts
@@ -3,11 +3,42 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import Sample from 'drug-report/data-models/sample';
 
-export default class ResultViewerListComponent extends Component {
+interface ResultViewerListArgs {
+samples: Array<Sample>
+}
+
+let timeoutId!: any;
+
+export default class ResultViewerListComponent extends Component<ResultViewerListArgs> {
     @tracked selectedSample!: Sample;
-    
+    @tracked searchInput = ""
+
+    @tracked filteredSamples = this.args.samples;
+    @tracked isSearchOpen = false;
+
+
     @action
     selectSample(sample: Sample){
         this.selectedSample = sample
     }
+
+    @action
+    toggleSearch(){
+        this.isSearchOpen = !this.isSearchOpen;
+        this.filteredSamples = this.args.samples
+    }
+
+    @action 
+    searchList(e: { target: { value: string } }){
+        clearTimeout(timeoutId)
+
+        timeoutId = setTimeout(function(this: any) {
+            this.handleSearch(e.target.value) ;
+        }.bind(this), 400)
+    }
+
+    handleSearch(this: any, searchKey: string){
+        this.filteredSamples = this.args.samples.filter((sample: Sample) => sample.name.toLowerCase().includes(searchKey.toLowerCase()))
+    }
 }
+
