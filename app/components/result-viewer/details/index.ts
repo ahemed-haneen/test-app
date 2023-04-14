cc84d5d6cd141cf1a5f7e33bd68d886f5ad540fc
ahemed-haneen <haneen.mavoor@gmail.com>
2023-04-14 07:18:05 +0530

    build(packages): setup ember table
    implement table custom function

--- app/components/result-viewer/details/index.ts
+++ app/components/result-viewer/details/index.ts
@@ -5,4 +5,23 @@ interface ResultViewerDetailsArgs {
     details: Sample
 }
 export default class ResultViewerDetailsComponent extends Component<ResultViewerDetailsArgs> {
+    columns = [{
+        name: "DRUG CLASS",
+        value: "drug_class"
+    }, 
+{
+    name: 'DRUG',
+    value: 'drugs',
+},
+{
+    name: 'CALL',
+    value: 'drugs',
+},{
+name: 'MUTATIONS',
+value: 'mutations'}]
+get rows(){
+    if (this.args.details)
+    return this.args.details.drug_call_result.drug_calls.map((row: {drug_class: string, drugs: Array<any>, mutations: Array<string>}) => { return {drug_class: row.drug_class, mutations: row.mutations, drugs: row.drugs}})
+else return []}
 }
+
